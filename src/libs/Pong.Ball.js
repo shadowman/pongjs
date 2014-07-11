a8cd02d1fa73cc8c7d587d5c9ea9d5f938933595
trikitrok <trikitrok@gmail.com>
2014-07-11 23:00:40 +0200

    Ball init function is really private now

--- src/libs/Pong.Ball.js
+++ src/libs/Pong.Ball.js
@@ -1,14 +1,14 @@
 var Ball = function(position, color, radius) {
-	var self = this;
+	init(this);
 	
-	self.render = function(context) {
+	this.render = function(context) {
 		var rctx = context.context;
 		rctx.beginPath();
-		rctx.fillStyle = self.color;
+		rctx.fillStyle = this.color;
 		rctx.arc(
-			self.position.e(1), 
-			self.position.e(2), 
-			self.radius, 
+			this.position.e(1), 
+			this.position.e(2), 
+			this.radius, 
 			0, 
 			2 * Math.PI, 
 			false
@@ -17,18 +17,14 @@ var Ball = function(position, color, radius) {
 		rctx.closePath();
 	};
 
-	self.update = function(dt) {
+	this.update = function(dt) {
 
 	};
 
-	self._init = function(position, color, radius) {
-		self.radius 	= radius || 10;
-		self.color 		= color ? color.toLowerCase() : '#ff0000';
-		self.position 	= position ? $V(position) : $V([0,0]);
-		self.velocity 	= $V([0, 0]);
-	};
-
-	self._init(position, color, radius);
-
-	return self;
+	function init (self) {
+		self.radius = radius || 10;
+		self.color = color ? color.toLowerCase() : '#ff0000';
+		self.position = position ? $V(position) : $V([0,0]);
+		self.velocity = $V([0, 0]);
+	}
 };
