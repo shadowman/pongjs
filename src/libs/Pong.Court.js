a272c3f67449c0aee7d5534f47d197d239dc64e3
Jose Enrique Rodriguez Huerta <staff.tsr@gmail.com>
2014-07-19 11:59:38 +0200

    Making idiomatic changes for the Court code

--- src/libs/Pong.Court.js
+++ src/libs/Pong.Court.js
@@ -1,48 +1,44 @@
 var Court = function(width, height, position) {
-	var self = this;
 	
-	self._init = function(width, height, position) {
-		self.width 		= width;
-		self.height 	= height;
-		self.position 	= position;
+	this.width 		= width;
+	this.height 	= height;
+	this.position 	= position;
+
+
+	this.render = function(rctxt) {
+		var self = this;
+		renderFloor(rctxt);
+		renderNet(rctxt);
+
+		function renderFloor (rctxt) {
+			var context = rctxt.context;
+
+			context.beginPath();
+			context.fillStyle = '#000000';
+			context.strokeStyle = '#FFFFFF';		
+			context.lineWidth = 5;
+			context.rect(
+				self.position.e(1), self.position.e(2), self.width, self.height
+			);
+			context.fill();
+			context.stroke();
+			context.closePath();
+		};
+		
+		function renderNet (rctxt) {
+			var context = rctxt.context;
+			context.beginPath();
+			context.strokeStyle = '#FFFFFF';		
+			context.lineWidth = 5;
+			context.moveTo(self.position.e(1) + self.width / 2, self.position.e(2));
+			context.lineTo(self.position.e(1) + self.width / 2, self.position.e(2) + height);
+			context.stroke();
+			context.closePath();
+		};
 	};
 
 
-	self.render = function(rctxt) {
-		self._renderFloor(rctxt);
-		self._renderNet(rctxt);
-	};
-
-	self._renderFloor = function(rctxt) {
-		var context = rctxt.context;
-
-		context.beginPath();
-		context.fillStyle = '#000000';
-		context.strokeStyle = '#FFFFFF';		
-		context.lineWidth = 5;
-		context.rect(
-			self.position.e(1), self.position.e(2), self.width, self.height
-		);
-		context.fill();
-		context.stroke();
-		context.closePath();
-	};
-	
-	self._renderNet = function(rctxt) {
-		var context = rctxt.context;
-		context.beginPath();
-		context.strokeStyle = '#FFFFFF';		
-		context.lineWidth = 5;
-		context.moveTo(self.position.e(1) + self.width / 2, self.position.e(2));
-		context.lineTo(self.position.e(1) + self.width / 2, self.position.e(2) + height);
-		context.stroke();
-		context.closePath();
-	};
-
-	self.udpate = function(dt) {
+	this.udpate = function(dt) {
 
 	};
-
-	self._init(width, height, position);
-	return self;
 };
