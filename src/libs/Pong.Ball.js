57eeec91008af717a0e2177da648b97c6239de3b
Jose Enrique Rodriguez Huerta <staff.tsr@gmail.com>
2014-07-19 11:52:15 +0200

    Making idiomatic changes for the Ball code

--- src/libs/Pong.Ball.js
+++ src/libs/Pong.Ball.js
@@ -1,3 +1,4 @@
+/*
 var Ball = function(position, color, radius) {
 	var self = this;
 	
@@ -32,3 +33,32 @@ var Ball = function(position, color, radius) {
 
 	return self;
 };
+*/
+
+var Ball = function(position, color, radius) {	
+	this.radius 	= radius || 10;
+	this.color 		= color ? color.toLowerCase() : '#ff0000';
+	this.position 	= position ? $V(position) : $V([0,0]);
+	this.velocity 	= $V([0, 0]);
+
+	this.render = function(context) {
+		var rctx = context.context;
+		rctx.beginPath();
+		rctx.fillStyle = this.color;
+		rctx.arc(
+			this.position.e(1), 
+			this.position.e(2), 
+			this.radius, 
+			0, 
+			2 * Math.PI, 
+			false
+		);
+		rctx.fill();
+		rctx.closePath();
+	};
+
+	this.update = function(dt) {
+
+	};
+};
+
